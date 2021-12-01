1808fbd687a4450798bbd89f9b5224ab5677810d
Marina Keller <73908142+surfmarina@users.noreply.github.com>
2021-12-01 14:29:27 -0800

    Update HeatExchanger.py

--- HeatExchanger.py
+++ HeatExchanger.py
@@ -1,27 +1,39 @@
 # compute the area for various heat exchanger geometries
+import math
+
 class HeatExchanger:
     def __init__(self):
         # no dimension can be greater than 2 times any other dimension
-        self.l = 2.5  # m - length of hx tubes
+        self.length = 2.5  # m - length of hx tubes
         self.x = 2  # m
         self.y = 2  # m
         self.diameter = 0.1  # meters
-    def numbertubes(self):
-        # given the entire dimensions, determine how many tubes can fit if aligned
-        xyarea = self.x*self.y
-        # let spacing btw tubes be 2 times the diameter
-        St = 2*self.diameter
-        
-        nt  # number of tubes
+    ## TODO Put values for air in the tables below given temp- some global variables
+    mass_flow = 10  # kg/s  mass flow in entire system
+    air_density = 1.154  # kg/m^3  density of air (external flow)
+    air_viscosity = 184.5E-7  # Ns/m^2
+    air_pr = 0.707
+
+    def number_tubes(self):
+        # given diameter and dimensions of hx
+        # find max number of tubes and update spacing to make it even
+        St = 2 * self.diameter  # initialise spacing as 2 times the diameter
+        Nx = math.floor(self.x/(2*self.diameter))  # round down to nearest int.
+        Ny = math.floor(self.y/(2*self.diameter))
+        # update spacing to make it even
+        Sx = self.x/(Nx*2)
+        Sy = self.y/(Ny*2)
+        return Nx, Ny, Sx, Sy
 
     # given the hx geometries and thermal properties, determine
     def velocity(self, air):
-        area = self.x * self.l  # find area external fluid flows thru
+        area = self.x * self.length  # find area external fluid flows thru
+        v = mass_flow/(air_density*)
 
-        mass_flow = 10  # kg/s  mass flow in entire system
-        air_density = 1.154  # kg/m^3  density of air (external flow)
-        air_viscosity = 184.5E-7  # Ns/m^2
-        air_pr = 0.707
-
-        velocity_air = mass_flow/(air_density*area)
-        print(velocity_air)
+        # if either velocity is above 100 m/s give a warning for compressible flow
+        iszero(vmax)
+        def is_zero(vmax):
+            if vmax >= 100 :
+                print
+                "WARNING: Air is travelling faster than 100 m/s"
+            return vmax
